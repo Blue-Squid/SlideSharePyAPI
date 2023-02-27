5c90f3b8dc89817efab688ff6c3678be4527916d
sboppana <sidduboppana@gmail.com>
2023-02-28 04:54:46 +0530

    Initial Commit of Slide Share Python API repo

---
+++ pyslideshare3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup (
+    name = "pyslideshare2",
+    description = "Python module for SlideShare v3",
+    long_description = """
+                        Python wrapper for SlideShare API, Originally authored by Prabhu Subramanian (prabhu.subramanian@gmail.com), 
+                        version 2 written by Marc Poulhiès (dkm@kataplop.net), 
+                        converted from Python2 to Python3 + minor bug fixes by BlueSquid 
+                       """,
+    version = "0.3",
+    author = 'Marc Poulhiès | BlueSquid',
+    author_email = 'dkm@kataplop.net',
+    url = "http://code.google.com/p/pyslideshare/",
+    maintainer = 'Germán Krauss',
+    maintainer_email = 'german@devartis.com',
+    license = "GPL,MIT",
+    packages = ['pyslideshare2'],
+    )
+
+
